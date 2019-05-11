21d1c7cb1311d50a5c443ef933bfab3ce3b50e58
Jiho2 <zihoyuno1@naver.com>
2019-05-11 20:26:40 +0900

    Version 0.1 Spageti source Warn : searchMine Algorithm problem...

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -7,21 +7,33 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     static final int BOARD_WIDTH_SMALL = 512;
     static final int BOARD_HEIGHT_SMALL = 512;
+    static final int CELL_SIZE_SMALL = 60;
+    static final int CELL_SIZE_MIDIUM = 70;
+    static final int CELL_SIZE_LARGE = 100;
+
 
     LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
             LinearLayout.LayoutParams.WRAP_CONTENT);
     LinearLayout gameBoard;
     Button btn1;
+    Mine btnArray[][];
+
+    static int cols, rows;
     int mineNum = 0;
     int cellSize;
+
+    int sRows = 0;
+    int sCols = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         gameBoard = (LinearLayout) findViewById(R.id.game_board);
-        cellSize = 100;
+        cellSize = CELL_SIZE_LARGE;
 
         btn1 = (Button) findViewById(R.id.startButton);
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -54,32 +66,26 @@ public class MainActivity extends AppCompatActivity {
         int width = gameBoard.getWidth();
         int height = gameBoard.getHeight();
 
-        int rows = width / cellSize;
-        int cols = height / cellSize;
+        rows = height / cellSize;
+        cols = width / cellSize;
 
-        ImageButton btnArray[][] = new ImageButton[cols][rows]; // 행열 크기의 버튼을 생성!
+        btnArray = new Mine[rows][cols]; // 행열 크기의 버튼을 생성!
 
         // 셀을 정했는데 그럼 버튼들 배치는 어떻게??
-        for (int i = 0; i < cols; i++) {
+        for (int i = 0; i < rows; i++) {
             LinearLayout myLayout = new LinearLayout(this);
             myLayout.setOrientation(LinearLayout.HORIZONTAL);
             gameBoard.addView(myLayout);
 
-            for (int j = 0; j < rows; j++) {
-                btnArray[i][j] = new ImageButton(this);
-                btnArray[i][j].setImageResource(R.drawable.cell);
-                btnArray[i][j].setMinimumWidth(cellSize);
-                btnArray[i][j].setMaxWidth(cellSize);
-                btnArray[i][j].setMinimumHeight(cellSize);
-                btnArray[i][j].setMaxHeight(cellSize);
-                btnArray[i][j].setAdjustViewBounds(true);
-                btnArray[i][j].setScaleType(ImageView.ScaleType.FIT_XY);
-                btnArray[i][j].setPadding(0,0,0,0);
-
+            for (int j = 0; j < cols; j++) {
+                btnArray[i][j] = new Mine(this, i, j);
                 btnArray[i][j].setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        ((ImageButton)v).setImageResource(R.drawable.cell_0);
+                        Mine block = (Mine) v;
+                        searchMine(block.row, block.col);
+                        block.initImage();
+                        Log.e("onClick", block.row + "," + block.col);
                     }
                 });
 
@@ -93,15 +99,95 @@ public class MainActivity extends AppCompatActivity {
         // 난이도에 따른 지뢰 설정
         if (true) {
             mineRatio = 0.15;
-            mineNum = (int) (cols * rows * mineRatio);
+            mineNum = (int) (rows * cols * mineRatio);
         }
 
         // 지뢰 랜덤 설치
+        Random rand = new Random();
+        int nMineNum = 0;
+
+        while (nMineNum < mineNum) {
+            nMineNum++;
+            int randRow = rand.nextInt(rows);
+            int randCol = rand.nextInt(cols);
+
+            // 만약 이미 지뢰가 있는 타일이면, 루프를 한번 더 돌리도록 정의
+            if (btnArray[randRow][randCol].isMine) {
+                nMineNum--;
+            }
+            btnArray[randRow][randCol].setIsMine(true);
+
+        }
+
 
         Log.e("Width Height Values", width + "," + height);
-        Log.e("Cols Rows Values", "cols = " + cols + "," + "rows = " + rows);
+        Log.e("Cols Rows Values", "rows = " + rows + "," + "cols = " + cols);
         Log.e("mineNum", String.format("mineNum : %d \n", mineNum));
     }
+
+    // 클릭 시, 연속되어 배치된 빈타일, 지뢰인접타일들을 모두 밝힌다.
+    // 어떻게 밝히는가? 지뢰가 아니라면 true로, Mine이 Click 된 것처럼 만든다.
+    // 이때 연산해서, Mine에 숫자를 부여해준다. // 혹은 게임 시작시 셋팅할때 밑바탕에 숫자를 깔아둘 수도 있기는 하다..
+    // 이런 마인 찾기 작업을 여기서 처리하는 게 맞는가.. 음..
+    // 지뢰가 있으면 True로 하는 식으로 해결해보자. 그러면 그 갯수를 가지고 파악할 수 있지 않을까? 아닌가 재귀 처리면 그걸 그렇게 인식하진 못하나..
+    public boolean searchMine(int x, int y) {
+
+        Log.e("searchMine", "x:" + x + ", " + "y:" + y);
+
+        // 이게.. 음..
+        if (x < 0 || y < 0 || x >= rows || y >= cols) {
+            Log.e("searchMine", "initiate" + rows + ", " + cols);
+            return false;
+        }
+        // 지뢰라면 동작 끝
+        else if (btnArray[x][y].isMine) {
+            Log.e("searchMine", "initiate2");
+            return true;
+        }
+        // 이미 방문한 타일이라면 동작 끝
+        else if (btnArray[x][y].isVisible){
+            return false;
+        }
+        // 이제 연산을 하자
+        else {
+            Log.e("searchMine", "initiate3");
+            btnArray[x][y].isVisible = true;
+
+            for (int i = x - 1; i <= x + 1; i++) {
+                for (int j = y - 1; j <= y + 1; j++) {
+
+                    if ((x == i && y == j) || i < 0 || j < 0 || i >= rows || j >= cols ){
+                        continue;
+                    }
+                    if(searchMine(i,j)) {
+                        btnArray[x][y].nearMine++;
+                        Log.e("nearMine Find", "x = " + x + ", " + y + " nearMine " + btnArray[x][y].nearMine);
+                    }
+                }
+            }
+
+            Log.e("nearMine", String.format("%s", btnArray[x][y].nearMine));
+
+            // 지뢰가 아니라면 세팅! 아니면 엥??
+            if (!btnArray[x][y].isMine) {
+                btnArray[x][y].initImage();
+            }
+
+            return false;
+        }
+
+    }
+
+
+    // set, get 메소드들 구현부
+
+    //get
+    static public int getCellSize() {
+
+        // 추후, 환경설정의 셀 선택 값에 따라 셀의 크기를 정하도록 구현
+        return CELL_SIZE_LARGE;
+    }
+
 
 }
 
--- app/src/main/java/com/example/minesweeper/Mine.java
+++ app/src/main/java/com/example/minesweeper/Mine.java
@@ -1,25 +1,171 @@
 package com.example.minesweeper;
 
 import android.content.Context;
-import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 /*
-* Mine 클래스
-* 각 타일들에 대한 정보들을 담고 있는 객체다.
-
+ * Mine 클래스
+ * 각 타일들에 대한 정보들을 담고 있는 객체다.
 
  */
 
 public class Mine extends ImageButton {
-    int row, col; // 열과 행
+    int cellSize;
+    int row, col; // row 행, col 열
+    int nearMine = 0;
+
     boolean isMine = false;
-    boolean isTouched = false;
+    boolean isVisible = false;
+    MineListener lis;
+
+    public class MineListener {
+        /*
+        @Override
+        public void onClick(View v) {
+
+            Log.e("myMine", isMine + "");
+
+            if (isMine) {
+                // 게임 패배 구현부
+
+            }
+            else{
+
+            }
+        }
+        */
+    }
 
     public Mine(Context context) {
         super(context);
+
+        cellSize = MainActivity.getCellSize();
+        setImageResource(R.drawable.cell);
+        setMinimumWidth(cellSize);
+        setMaxWidth(cellSize);
+        setMinimumHeight(cellSize);
+        setMaxHeight(cellSize);
+        setAdjustViewBounds(true);
+        setScaleType(ImageView.ScaleType.FIT_XY);
+        setPadding(0, 0, 0, 0);
+
+        //lis = new MineListener();
+        //setOnClickListener(lis);
     }
-    public Mine(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    public Mine(Context context, int x, int y) {
+        super(context);
+
+        cellSize = MainActivity.getCellSize();
+        setImageResource(R.drawable.cell);
+        setMinimumWidth(cellSize);
+        setMaxWidth(cellSize);
+        setMinimumHeight(cellSize);
+        setMaxHeight(cellSize);
+        setAdjustViewBounds(true);
+        setScaleType(ImageView.ScaleType.FIT_XY);
+        setPadding(0, 0, 0, 0);
+
+        row = x;
+        col = y;
+
+        //lis = new MineListener();
+        //setOnClickListener(lis);
     }
+    public void checkMine(int x, int y) {
+
+
+    }
+
+
+/*
+    public boolean searchMine(int x, int y) {
+
+        // 이게.. 음..
+        if (x < 0 || y < 0 || x > rows || y > cols) {
+            return false;
+        }
+        // 이미 밝혀진 타일이면 false, 수행 안함
+        else if (btnArray[x][y].isVisible) {
+            return false;
+        }
+        // 이제 연산을 하자
+        else {
+            // 각 8번의 재귀함수 속에서... 체크해서 값 추가해야함, 근데 재귀에서 계속 올라오면 결국은 죄다 true 아니겠는가?
+            // 주위 8개 체크하는 건 다른 함수로 체크하자?  아닌데..
+
+            for (int i = x - 1; i <= x + 1; i++) {
+                for (int j = y - 1; j <= y + 1; j++) {
+
+                    if (x == i && y == j) {
+                        continue;
+                    }
+
+                    if (btnArray[i][j].isMine) {
+                        btnArray[x][y].nearMine++;
+                    }
+                }
+            }
+
+            if (!btnArray[x][y].isMine){
+                btnArray[x][y].initImage();
+                searchMine(x - 1, y + 1);
+                searchMine(x, y + 1);
+                searchMine(x + 1, y + 1);
+                searchMine(x + 1, y);
+                searchMine(x + 1, y - 1);
+                searchMine(x, y - 1);
+                searchMine(x - 1, y - 1);
+                searchMine(x - 1, y);
+
+                return true;
+            }
+
+            return false;
+        }
+
+    }*/
+
+    // init
+    public void initImage(){
+        Log.e("InitImage", "nearMine" + nearMine);
+        if (isMine) {
+            // 지뢰를 골랐다.
+            setImageResource(R.drawable.mine);
+
+        } else {
+            if (nearMine == 0) {
+                setImageResource(R.drawable.cell_0);
+            } else if (nearMine == 1) {
+                setImageResource(R.drawable.cell_1);
+            } else if (nearMine == 2) {
+                setImageResource(R.drawable.cell_2);
+            } else if (nearMine == 3) {
+                setImageResource(R.drawable.cell_3);
+            } else if (nearMine == 4) {
+                setImageResource(R.drawable.cell_4);
+            } else if (nearMine == 5) {
+                setImageResource(R.drawable.cell_5);
+            } else {
+                setImageResource(R.drawable.cell);
+            }
+        }
+    }
+
+    // set, get 메소드 구현부
+    public void setImage(int id){
+        this.setImageResource(id);
+    }
+
+
+
+    // set 메소드
+    public void setIsMine(boolean isMine) {
+        this.isMine = isMine;
+    }
+
+
+
 }
